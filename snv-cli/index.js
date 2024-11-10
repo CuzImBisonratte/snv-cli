66c99503cb5e533cb76134d1677b5b29d4e15505
Konstantin Protzen <code@kondev.de>
2024-11-10 17:58:12 +0100

    🐛 Fix showing "Path: action" on exiting folder-action-menu

--- snv-cli/index.js
+++ snv-cli/index.js
@@ -349,6 +349,7 @@ async function filemanager() {
                     });
                     continue;
             }
+            continue;
         }
         console.clear();
         showBanner();
